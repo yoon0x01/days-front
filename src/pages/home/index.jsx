f8b4f633f30740688e95d82a4a4b4f3630bbc75b
yoon0x01 <yoon0x01@gmail.com>
2026-01-26 22:01:57 +0900

    독바 아이콘 이미지 변경

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return(
+        <>
+            <h1 className="text-red-500 text-3xl font-bold underline">
+                tailwind 테스트중
+            </h1>
+        </>
+    )
+}
